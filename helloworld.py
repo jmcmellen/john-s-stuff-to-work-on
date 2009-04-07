4a00f21a2d000296830fc92e5904e025cc073de2
John McMellen <jmcmellen@gmail.com>
2009-04-06 21:19:36 -0500

    third commit

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 class MainPage(webapp.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type']= 'text/plain'
+    self.response.headers['Content-Type']= 'text/html'
     self.response.out.write('''
 <html><head><title>My app</title></head>
 <body>
