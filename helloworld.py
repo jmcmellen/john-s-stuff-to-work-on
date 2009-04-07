a2fff62d07d405a9f22e4c6455718003a003712c
John McMellen <jmcmellen@gmail.com>
2009-04-06 21:16:52 -0500

    second commit

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type']= 'text/plain'
+    self.response.out.write('''
+<html><head><title>My app</title></head>
+<body>
+<h1>Hello, webapp World!</h1>
+<p>What\'s going on?</p>
+</body>
+</html>''')
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+if __name__ == "__main__":
+  main()
+
