3928f9a0f1a6fbeb02cad8d1af0d168cb3e0f821
John McMellen <jmcmellen@gmail.com>
2010-11-22 17:28:00 -0600

    Added makeDTMF function

--- audiogen.py
+++ audiogen.py
@@ -1,5 +1,34 @@
 import math, struct, random, array
 
+def makeDTMF(sequence, duration, pause, peakLevel, sampRate, sampWidth, numCh):
+    dtmf_values = {
+        '1':(1209, 697), '2':(1336, 697), '3':(1477, 697), 'A':(1633, 697),
+	'4':(1209, 770), '5':(1336, 770), '6':(1477, 770), 'B':(1633, 770),
+	'7':(1209, 852), '8':(1336, 852), '9':(1477, 852), 'C':(1633, 852),
+	'*':(1209, 941), '0':(1336, 941), '#':(1477, 941), 'D':(1633, 941) }
+    data_out = ''
+    phase = 0 * math.pi
+    level = math.pow(10, (float(peakLevel - 6) / 20)) * 32767
+
+    for a in sequence:
+	#print a, peakLevel - 6
+	if a == ' ':
+	    data_out += generateSimplePCMToneData(1000, 1000, sampRate, pause, 
+		    sampWidth, -110, numCh)
+	else:
+	    freq1, freq2 = dtmf_values[a]
+	    #print freq1, freq2
+	    for i in range(0, int(math.floor(sampRate * duration))):
+		for ch in range(numCh):
+		    sample =  int(math.ceil( level * math.sin(
+	                   (freq1 * 2 * math.pi * i)/ sampRate + phase) )) 
+		    sample += int(math.ceil( level * math.sin(
+			   (freq2 * 2 * math.pi * i)/ sampRate + phase) ))
+		    #print sample
+		    data_out += struct.pack('<h', sample)
+	    
+    return data_out
+
 def applyLinearFade(startVal, endVal, numCh, sampWidth, data):
     endLvl = math.pow(10, (float(endVal) / 20))
     startLvl = math.pow(10, (float(startVal) / 20))
@@ -43,12 +72,14 @@ def generateSimplePCMToneData(startfreq, endfreq, sampRate, duration, sampWidth,
     level = math.pow(10, (float(peakLevel) / 20)) * 32767 #Should depend on sampWidth
     pcm_data = ''
     freq = startfreq
-
+    #print duration * sampRate
 
     for i in range(0, int(math.floor(sampRate * duration))):
         for ch in range(numCh):
-	        pcm_data += struct.pack('<h', int(math.ceil( level * math.sin(
-	                   (freq * 2 * math.pi * i)/ sampRate + phase) )))
+	    sample =  int(( level * math.sin(
+	                   (freq * 2 * math.pi * i)/ sampRate + phase) ))
+	    #print sample
+	    pcm_data += struct.pack('<h', sample)
 
     return pcm_data
 
@@ -94,7 +125,7 @@ def generateEASpcmData(org, event, fips, eventDuration, timestamp, stationId, sa
 	                 numCh, preamble + message.upper())
     eom = generateAFSKpcmData(markF, spaceF, bitrate, sampRate, sampWidth, peakLevel,
 	                 numCh, preamble + endOfMessage)
-    silence = generateSimplePCMToneData(10000, sampRate, 1, sampWidth, -94, numCh)
+    silence = generateSimplePCMToneData(10000, 10000, sampRate, 1, sampWidth, -94, numCh)
     
     pcm_data = silence + silence
 
@@ -108,22 +139,26 @@ def generateEASpcmData(org, event, fips, eventDuration, timestamp, stationId, sa
 if __name__ == "__main__":
     import wave, time
 
-    freq = 1000
-    sampRate = 44100
+    freq = 1209
+    sampRate = 48000
     duration = 10
     sampWidth = 16
-    peakLevel = -3
+    peakLevel = -20
     numCh = 2
     now = time.gmtime()
     timestamp = time.strftime('%j%H%M', now) 
+    #dtmf_seq = ' 1 2 3 4 5 6 7 8 9 A B C D * # '
+    dtmf_seq = '      1   4 1 7   8 3 6   6 2 5 5      '
+    #dtmf_seq = '1234'
+    #dtmf_seq = '1'
     #print timestamp
 
-    #data = generateEASpcmData('EAS', 'RWT', '029091', '0030', timestamp, 'KXYZ/FM', sampRate, 
-	#    sampWidth, peakLevel, numCh)
-    data = generateSimplePCMToneData(freq, freq, sampRate, duration, sampWidth, peakLevel, numCh)
-    data = applyLinearFade(-100, 0, numCh, sampWidth, data)
+    data = generateEASpcmData('EAS', 'RWT', '029091', '0030', timestamp, 'KXYZ/FM', sampRate, 
+	    sampWidth, peakLevel, numCh)
+    #data = generateSimplePCMToneData(freq, freq, sampRate, duration, sampWidth, peakLevel, numCh)
+    #data = applyLinearFade(-100, 0, numCh, sampWidth, data)
     #data = changeLevelPCMdata(sampRate, sampWidth, -6, numCh, data)
-
+    #data = makeDTMF(dtmf_seq, 0.3, 0.08, peakLevel, sampRate, sampWidth, numCh)
     file = wave.open('testfile.wav', 'wb')
     file.setparams( (numCh, sampWidth/8 , sampRate, duration * sampRate, 'NONE', '') )
     file.writeframes(data)
